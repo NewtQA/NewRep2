242c64846d4b95aad200036e466bcef0b8d93dfa
NewtQA <alshas@newtglobal.com>
2018-05-31 16:43:11 +0530

    First commit

---
+++ src/test/java/com/mercurytours/com/jiraticketraise/Mokka.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.mercurytours.com.jiraticketraise;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * @author Sreelakshmi Totkar
+ *
+ */
+public class Mokka {
+	@Test
+	public void method(){
+     Assert.assertEquals(13, 14);
+	}
+
+}
